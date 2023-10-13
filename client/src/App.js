52f58985e300f7d2ff7d663997ccaa8d3e170649
kim_jihyung <sy33002@naver.com>
2023-10-13 15:56:00 +0900

    fear: server 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,23 @@
+import { useEffect, useState } from 'react';
 import Todo from './components/Todo';
 import AddTodo from './components/AddTodo';
-import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      title: 'my todo1',
-      done: false,
-    },
-    {
-      id: 2,
-      title: 'my todo2',
-      done: false,
-    },
-    {
-      id: 3,
-      title: 'my todo3',
-      done: true,
-    },
-    {
-      id: 4,
-      title: 'my todo4',
-      done: false,
-    },
-  ]);
+  console.log(process.env.REACT_APP_DB_HOST); // 환경변수 불러옴
+  const [todoItems, setTodoItems] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async() => {
+      const res = await axios.get(`${process.env.REACT_APP_DB_HOST}/todos`);
+      setTodoItems(res.data);
+    };
+
+    getTodos();
+  }, []);
 
   // todoItems 상태에 새로운 투두를 추가하는 일
   const addItem = (newItem) => {
-    // concat version!!
-    // const newTodoList = todoItems.concat({id: todoItems.length+1, title: newItem.title, done: false});
-    // setTodoItems(newTodoList)
     newItem.id = todoItems.length + 1;
     newItem.done = false;
     setTodoItems([...todoItems, newItem]);
@@ -42,12 +29,12 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div>
+       <h1 className="text-3xl font-bold underline">todoList</h1>
       <AddTodo addItem={addItem}/>
       {/* todoItems 반복, props 데이터 넘김 */}
       {todoItems.map((item) => (
-        <Todo key={item.id} item={item} 
-        deleteItem={() => deleteItem(item.id)}
+        <Todo key={item.id} item={item} deleteItem={() => deleteItem(item.id)}
         />
       ))}
     </div>
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // checkbox와 label을 렌더링하는 투두
-export default function Todo({item, deleteItem}) {
+export default function Todo({ item, deleteItem }) {
+  const [todoItem, setTodoItem] = useState(item);
   const { id, title, done } = item;
+  const [readOnly, setReadOnly] = useState(true);
 
-  const onDeleteButtonClick = (id) => {
-    console.log(id);
-    deleteItem(id);
+  const onDeleteButtonClick = () => {
+    deleteItem(todoItem);
+  }
+
+  //  title 클릭하면 readOnly를 수정 가능하도록!
+  const offReadOnlyMode = () => {
+    setReadOnly(false);
+  };
+  // enter 키 누르면 readOnly true로 변경
+  const editKeyEventHandler =(e) => {
+    if (e.key === 'Enter') {
+      setReadOnly(true);
+    }
+  };
+  // title 수정
+  // rest 파라미터
+  const editEventHandler = (e) => {
+    const {title, ...rest} = todoItem;
+    setTodoItem({
+      title: e.target.value,
+      ...rest,
+    });
+  };
+  //checkbox 상태 업데이트
+  const checkboxEventHandler = (e) => {
+    const {done, ...rest} = todoItem
+    setTodoItem( {
+      done: e.target.checked, 
+      ...rest,
+    })
   }
   return (
     <div>
@@ -14,9 +43,18 @@ export default function Todo({item, deleteItem}) {
         type='checkbox' 
         name={`todo${id}`} 
         id={`todo${id}`} 
-        defaultChecked={done}/>
-      <label htmlFor={id}>{title}</label>
-      <button onClick={() => onDeleteButtonClick(item.id)}>DELETE</button>
+        defaultChecked={done}
+        onChange={checkboxEventHandler}
+      />
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input type='text'
+        value={todoItem.title} 
+        readOnly={readOnly} 
+        onClick={offReadOnlyMode}
+        onChange={editEventHandler}
+        onKeyDown={editKeyEventHandler}
+      />
+      <button onClick={onDeleteButtonClick}>DELETE</button>
     </div>
   )
 }
